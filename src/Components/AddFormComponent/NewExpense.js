80705446cc32cf3e43ef96ec8a998d3ac79e9f0e
shiwani295 <shiwanitiwarishiwani@gmail.com>
2023-01-15 12:55:01 +0530

    challanges on condition and add cencel button

--- src/Components/AddFormComponent/NewExpense.js
+++ src/Components/AddFormComponent/NewExpense.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense(props) {
@@ -9,11 +9,32 @@ function NewExpense(props) {
     };
     //come from parent(app.js ) and then send data (expenseData) to again app.js
     props.onAddExpense(expenseData);
+    setEditing(false);
   };
+
+  const [isEditing, setEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setEditing(false);
+  };
+
   return (
     <div>
       {/* here you are pass the data child(ExpenseForm) to parent(NewExpense) */}
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <div className="Add-Expense">
+          <button onClick={startEditingHandler}>Add New Expense</button>
+        </div>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 }
